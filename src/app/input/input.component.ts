201cd8ee90247d92224762ee0e49dcddce03c8c8
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-01 14:08:25 +0200

    ♻️ Refactor form logic into input component

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
+  @Input() control: any
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
+
