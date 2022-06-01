1dc269ffb8f40e4140f1e6fe3dc2a9382120a46d
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-01 16:35:51 +0200

    ⚡️ Add reset functionality

--- src/app/date-form-control.ts
+++ src/app/date-form-control.ts
@@ -1,7 +1,12 @@
 import { FormControl } from "@angular/forms";
 
 export class DateFormControl extends FormControl {
-    override setValue(value: string, options: any) {
+    override setValue(value: string | null, options: any) {
+        if (!value) {
+            super.setValue('', {...options, emitModelToViewChange: true})
+            return;
+        }
+
         if (value.match(/[^0-9|\/]/gi)) {
             super.setValue(this.value, {...options, emitModelToViewChange: true})
             return;
