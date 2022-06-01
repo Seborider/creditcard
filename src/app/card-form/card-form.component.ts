e71d3795b98a2d96d7f529f199c46da5fee0e00d
SeboMyr <80338167+Seborider@users.noreply.github.com>
2022-06-01 15:00:37 +0200

    🎨 Add more validation for other Inputs

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -13,9 +13,21 @@ export class CardFormComponent implements OnInit {
       Validators.minLength(3),
       // Validators.maxLength(5)
     ]),
-    cardNumber: new FormControl(''),
-    expiration: new FormControl(''),
-    securityCode: new FormControl('')
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+
+    ]),
+    expiration: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    securityCode: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3)
+    ])
   })
 
   constructor() {
